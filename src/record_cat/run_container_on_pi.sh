728da9ec8e418222a46b5eab14a879b27d282443
Doctores Pi <pi@doctores-pi.com>
2020-10-10 17:31:30 +0100

    Add non-root user with access to webcam.

--- src/record_cat/run_container_on_pi.sh
+++ src/record_cat/run_container_on_pi.sh
@@ -3,7 +3,7 @@
 data_dir='/home/pi/intellicatflap/data'
 
 if [ $2 = 'it' ]; then
-    docker run -v $data_dir:/app/data -it --device /dev/video0 catdetector:$1 /bin/bash
+    docker run -v $data_dir:/app/data -it --device=/dev/video0:/dev/video0 catdetector:$1 /bin/bash
 elif [ $2 = 'app' ]; then
-    docker run -v $data_dir:/app/data --device /dev/video0 catdetector:$1
+    docker run -v $data_dir:/app/data --device=/dev/video0:/dev/video0 catdetector:$1
 fi
