f0a9cd28ad4360b19e34cd3b0e8d4dd0390bdb8b
ThiesDS <sven_thies@web.de>
2020-11-29 18:01:38 +0100

    Add test script to push images to gcs bucket.

--- src/record_cat/app/push_images_to_gcs.py
+++ src/record_cat/app/push_images_to_gcs.py
@@ -1,1 +1,13 @@
+from google.cloud import storage
 
+# Setting credentials using the downloaded JSON file
+client = storage.Client.from_service_account_json(json_credentials_path='../../../config/intellicatflap-ccc7c20cdc79.json')
+
+# Creating bucket object
+bucket = client.get_bucket('intellicatflap')
+print(bucket)
+# Name of the object to be stored in the bucket
+object_name_in_gcs_bucket = bucket.blob('raw/test_datetimewms.jpg')
+print(object_name_in_gcs_bucket)
+# Name of the object in local file system
+object_name_in_gcs_bucket.upload_from_filename('../../../data/img_1.jpg')
