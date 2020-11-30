de617672cf5dcf0d8db8028acf5eed07f4109a7e
ThiesDS <sven_thies@web.de>
2020-11-30 18:59:10 +0100

    Add upload of files.

--- src/gcs_sync/app/push_images_to_gcs.py
+++ src/gcs_sync/app/push_images_to_gcs.py
@@ -4,25 +4,26 @@ from google.cloud import storage
 # Get current working directory
 cwd = os.getcwd()
 
-# Path of local images
-img_path = cwd + '/data/'
-
-# Path of gcs images (raw)
-gcs_path = 'raw/'
+# Paths
+local_dir = cwd + '/data/'
+gcs_dir = 'raw/'
 
 # Set credentials using the downloaded JSON file and get bucket object
 client = storage.Client.from_service_account_json(json_credentials_path='gcs_serviceaccount.json')
-#bucket = client.get_bucket('intellicatflap')
+bucket = client.get_bucket('intellicatflap')
 
 # Get all images in data folder
 files = os.listdir(img_path)
-print(files)
 
 # Upload and delete file from local storage
 for file in files:
-    # Create object and upload
-    object_name = gcs_path + 'img_' + timestamp + '.jpg'
-    print('In Loop:')
-    print(object_name)
-    #blob = bucket.blob(object_name)
-    #blob.upload_from_filename(file)
+    # Upload only jpg images
+    if file.endswith(".jpg"):
+        # Create file paths
+        local_file_path = local_dir + file
+        gcs_file_path = gcs_dir + file
+        # Upload from local to gcs
+        blob = bucket.blob(gcs_file_path)
+        blob.upload_from_filename(local_file_path)
+        # DEBUG
+        print('Uploaded file: ' + file)
