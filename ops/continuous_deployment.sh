9dd20c6af175ccd2f09d0456190a7173cabe2b44
ThiesDS <sven_thies@web.de>
2020-12-05 08:30:07 +0100

    Add pi configuration.

--- ops/continuous_deployment.sh
+++ ops/continuous_deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go to root directory of project (from whereever you are)
-cd /home/doctore/intellicatflap/
+cd /home/pi/intellicatflap/
 
 # Pull from pi_deployment branch of repository
 git fetch --all && git checkout "pi_deployment" && git pull 
