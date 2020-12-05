1b0afaa0a23316fad689e90aee45e570ef373475
ThiesDS <sven_thies@web.de>
2020-12-05 08:26:25 +0100

    Test

--- ops/continuous_deployment.sh
+++ ops/continuous_deployment.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Go to root directory of project (from whereever you are)
-cd /home/pi/intellicatflap/
+cd /home/doctore/intellicatflap/
 
 # Pull from pi_deployment branch of repository
 git fetch --all && git checkout "pi_deployment" && git pull 
+
+echo "hello"
