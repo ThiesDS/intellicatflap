31f84e6c648483469bcd747f8e308e0a37d2ea18
Sven Thies <sven.thies@infomotion.de>
2021-01-18 19:40:00 +0100

    Fix bug gcs sync.

--- src/gcs_sync/app/main.py
+++ src/gcs_sync/app/main.py
@@ -5,16 +5,21 @@ import gcs_sync
 # Current working directory
 cwd = os.getcwd()
 
+detection_log_file = cwd + '/data/cat_detection.log'
+operation_log_file = cwd + '/logs/operation.log'
+
 # Loop every 3 hours and uplaod files to gcs
-for i in range(0,9999):
+while True:
     # Wait 3 hours: Camera will take photos
     time.sleep(5)
     
     # Upload images and detections files to gcs
     gcs_sync.upload_images_to_gcs(local_dir=cwd + '/data/',gcs_dir='raw/operations/')
 
-    # Upload operation log files    
-    gcs_sync.upload_file_to_gcs(local_file=cwd + '/data/cat_detection.log',gcs_file='raw/detections/detection_raw_cv2.log')
+    # Upload operation log files
+    if os.path.exists(detection_log_file):
+        gcs_sync.upload_file_to_gcs(local_file=detection_log_file,gcs_file='raw/detections/detection_raw_cv2.log')
 
     # Upload operation log files
-    gcs_sync.upload_file_to_gcs(local_file=cwd + '/logs/operation.log',gcs_file='raw/ops/operation.log')
+    if os.path.exists(operation_log_file):
+        gcs_sync.upload_file_to_gcs(local_file=operation_log_file,gcs_file='raw/ops/operation.log')
