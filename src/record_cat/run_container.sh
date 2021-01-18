90d6b28574f4fa7d2df94ec1792e22e0674b4df0
Doctores Pi <pi@doctores-pi.com>
2021-01-18 19:50:37 +0100

    change run container.

--- src/record_cat/run_container.sh
+++ src/record_cat/run_container.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 data_dir=$HOME/intellicatflap/data
+models_dir=$HOME/intellicatflap/models
 
 if [ $3 = 'build_true' ]; then
 	docker build -t catdetector:$1 .
 fi
 
 if [ $2 = 'it' ]; then
-    docker run -v $data_dir:/app/data -it --device=/dev/video0:/dev/video0 catdetector:$1 /bin/bash
+    docker run -v $data_dir:/app/data -v $models_dir:/app/models -it --device=/dev/video0:/dev/video0 catdetector:$1 /bin/bash
 elif [ $2 = 'app' ]; then
-    docker run -v $data_dir:/app/data --device=/dev/video0:/dev/video0 catdetector:$1
+    docker run -v $data_dir:/app/data -v $models_dir:/app/models --device=/dev/video0:/dev/video0 catdetector:$1
 else 
 	echo "Error: Argument 2 must be either it or app."
 fi
