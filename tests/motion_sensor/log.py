6ba4725f99b5f9eac0b33dfe93487ad127677755
Sven Thies <sven.thies@infomotion.de>
2021-05-29 09:53:07 +0200

    Add logging module and modify test of motion sensor.

---
+++ tests/motion_sensor/log.py
@@ -0,0 +1,61 @@
+import logging
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+
+
+# Logger config
+FORMATTER = logging.Formatter("%(asctime)s|%(name)s|%(levelname)s|%(funcName)s:%(lineno)d|%(message)s")
+
+
+# Define console handler
+def get_console_handler():
+    """
+        Get console handler.
+    """
+
+    # Define console handler and formatting
+    console_handler = logging.streamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+
+    return console_handler
+
+# Define google cloud platform handler
+def get_gcp_handler():
+    """
+        Get gcp handler for logging in gcp project 'intellifactflap'
+    """
+
+    # Define handler
+    client = google.cloud.logging.Client(project="intellifactflap")
+    cloud_handler = CloudLoggingHandler(client, name="intellicatflap_log")
+    cloud_handler.setFormatter(FORMATTER)
+
+    return cloud_handler
+
+# Main function to get logger
+def get_logger(name: str, log_level: str):
+    """
+        Main functionto get a logger with a specified log level. 
+    """
+
+    # Get logger
+    logger = logging.getLogger(name)
+
+    # Set log level
+    if log_level == 'DEBUG':
+        logger.setLevel(logging.DEBUG)
+    elif log_level == 'INFO':
+        logger.setLevel(logging.INFO)
+    elif log_level == 'WARN':
+        logger.setLevel(logging.WARN)
+    elif log_level == 'ERROR':
+        logger.setLevel(logging.ERROR)
+    elif log_level  == 'CRITICAL':
+        logger.setLevel(logging.CRITICAL)
+
+    # Add handler
+    logger.addHandler(get_console_handler())
+    #logger.addHandler(get_google_handler())
+
+    return logger
+
--- tests/motion_sensor/test.py
+++ tests/motion_sensor/test.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
- 
+import log
+
+
+# Get logger
+logger = log.get_logger(__name__, log_level='DEBUG')
 
 # config
 SENSOR_PIN = 7
@@ -12,12 +16,14 @@ def callback(channel):
     print('Cat movement detected!')
 
 # start event/motion detection
+logger.info('Start test motion detection.')
 try:
     while True:
-        GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=callback)
+        #GPIO.add_event_detect(SENSOR_PIN , GPIO.RISING, callback=callback)
+        logger.info('Test output.')
         time.sleep(5)
 except KeyboardInterrupt:
     print("Quit testing ...")
 
 # cleanup GPIO stuff
-GPIO.cleanup()
+#GPIO.cleanup()
